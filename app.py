9c09a8538bb6bc7a2191512dff06bb2498a16928
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2023-11-22 19:21:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -304,4 +304,4 @@ api.add_resource(showAllPilot, '/showallPilot')
 api.add_resource(makeOffline, '/makeoffline')
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000,host="0.0.0.0",threaded=True)
+    app.run(debug=False,port=5000,host="0.0.0.0",threaded=True)
